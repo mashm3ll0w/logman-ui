8338ec977b6d3282f1aa900858bc4ed728b4338a
devngugi <philemonngugi18@gmail.com>
2025-05-08 13:13:45 +0300

    update owner

--- src/components/FooterBar.vue
+++ src/components/FooterBar.vue
@@ -9,7 +9,7 @@ const year = new Date().getFullYear()
   <footer class="py-2 px-6" :class="containerMaxW">
     <BaseLevel>
       <div class="text-center md:text-left">
-        <b>&copy;{{ year }},<a href="https://github.com/DevNgugi/">DevNgugi</a></b>
+        <b>&copy;{{ year }}, <a href="https://github.com/DevNgugi/">DevNgugi</a></b>
         
       </div>
       <div class="md:py-2">
