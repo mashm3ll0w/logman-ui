a1de70ab7623465818895580128836f29fcb2ad0
Ngugi Philemon <113933822+devngugi@users.noreply.github.com>
2025-08-26 22:28:21 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [vue()],
-  base: '/logman/'
+  base: '/logman/',
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
